d892e7fa9a25b4d80d1c83b2373ab7b277fb6f46
Tastchunpo <Tastchunpo@Tastchunpo-PC>
2016-04-29 08:32:56 +0800

    16-4-29

--- src/main/java/com/jd/weather/controller/RoadController.java
+++ src/main/java/com/jd/weather/controller/RoadController.java
@@ -1,13 +1,31 @@
 package com.jd.weather.controller;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
+@RequestMapping(value="/weath")
 public class RoadController {
+	public static Logger logger=LoggerFactory.getLogger(RoadController.class);
 	@RequestMapping(value={"/weather2","/weather3"})
+	public ModelAndView index(){
+		logger.info("访问come首页info");
+		logger.debug("访问come首页info");
+		logger.warn("访问come首页info");
+		logger.error("访问come首页info");
+		return new ModelAndView("RoadController");
+	}
 	public String roadMessage(){
+		
 		return "大堵车";
+	
 	}
 }
 
